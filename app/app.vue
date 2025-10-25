4ac70a218ecb73d1f9ee545caf9bfefb4bfd816e
Andrew <eralinkd@gmail.com>
2025-10-25 21:03:14 +0200

    commit

--- app/app.vue
+++ app/app.vue
@@ -10,12 +10,12 @@ const globals = await useGlobals()
 const { isOpen, close } = useFeedbackModal()
 
 useHead({
-  title: globals.value.meta.title,
+  title: globals?.value?.meta?.title,
   meta: [
-    { name: 'description', content: globals.value.meta.seo.description },
-    { property: 'og:title', content: globals.value.meta.seo.ogTitle },
-    { property: 'og:description', content: globals.value.meta.seo.ogDescription },
-    { property: 'og:image', content: globals.value.meta.seo.ogImage },
+    { name: 'description', content: globals?.value?.meta?.seo?.description },
+    { property: 'og:title', content: globals?.value?.meta?.seo?.ogTitle },
+    { property: 'og:description', content: globals?.value?.meta?.seo?.ogDescription },
+    { property: 'og:image', content: globals?.value?.meta?.seo?.ogImage },
   ],
   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
 })
