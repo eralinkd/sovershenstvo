01e8f4e2f1a684e60a6688bc273fe631854ba0fd
Andrew <eralinkd@gmail.com>
2025-11-03 22:23:12 +0100

    f

--- app/api.js
+++ app/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = 'https://api.unyto.ru:8443'
-const SITE_TOKEN = '184df047-2563-4.228-bbc0-61d8cc528bfd'
+const SITE_TOKEN = '184df047-2563-4228-bbc0-61d8cc528bfd'
 
 export const api = {
   async pushWebsiteQuestionnaire(websiteQuestionnaire) {
@@ -19,7 +19,7 @@ export const api = {
         throw new Error(`HTTP error! status: ${response.status}`)
       }
 
-      return await response.json()
+      return await response
     } catch (error) {
       console.error('API request failed:', error)
       throw error
