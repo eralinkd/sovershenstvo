4d1b7bd2a1cc7c8941b528441f95ab5715077044
eralinkd <eralinkd@gmail.com>
2026-03-18 23:34:02 +0100

    vercel fix

--- server/plugins/globals-refresh.js
+++ server/plugins/globals-refresh.js
@@ -4,9 +4,9 @@ import { refreshGlobalsFromDb, startGlobalsRefresh } from '../utils/globals-stor
 import { refreshMainFromDb, startMainRefresh } from '../utils/main-store'
 import { refreshPricingFromDb, startPricingRefresh } from '../utils/pricing-store'
 
-export default defineNitroPlugin(() => {
-  console.log('DATABASE_URL starts with:', process.env.DATABASE_URL?.slice(0, 20))
-  Promise.all([
+export default defineNitroPlugin(async () => {
+  // remove this after vercel deploy
+  await Promise.all([
     refreshGlobalsFromDb(),
     refreshFaqFromDb(),
     refreshFooterFromDb(),
