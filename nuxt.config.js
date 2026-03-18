1911a80ce160ffac698e8ca1d705438d7d2d0125
eralinkd <eralinkd@gmail.com>
2026-03-18 23:25:24 +0100

    f

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,10 @@ export default defineNuxtConfig({
 
   modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/icon', '@nuxt/image', '@nuxt/fonts'],
 
+  nitro: {
+    preset: 'vercel',
+  },
+
   image: {
     quality: 80,
     format: ['webp'],
